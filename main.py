4bbae5042fa72b61b8220ec5e57ce0c71f22d867
TimePrecursor <131825199+TimePrecursor@users.noreply.github.com>
2023-06-19 22:19:36 +0000

    Some imports and definitions of key components.

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("e")
+import fcntl, termios, struct
+#import random
+import time
+import os
+
+def slep1():
+  time.sleep(1)
+def slep2():
+  time.sleep(2)
+def slep3():
+  time.sleep(3)
+def clr():
+  os.system('clear')
+def terminal_size():
+      th, tw, hp, wp = struct.unpack('HHHH',
+          fcntl.ioctl(0, termios.TIOCGWINSZ,
+          struct.pack('HHHH', 0, 0, 0, 0)))
+      global width
+      width = tw
+      return tw, th
+terminal_size()
+
