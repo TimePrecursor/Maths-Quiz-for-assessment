a379d8f3b547af875adac8423a139747055a267b
TimePrecursor <131825199+TimePrecursor@users.noreply.github.com>
2023-06-20 03:35:13 +0000

    started working on difficulty choosing

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def slep2():
   time.sleep(2)
 def slep3():
   time.sleep(3)
-def clr():
-  os.system('clear')
+
+
 def terminal_size():
       th, tw, hp, wp = struct.unpack('HHHH',
           fcntl.ioctl(0, termios.TIOCGWINSZ,
@@ -20,12 +20,16 @@ def terminal_size():
       return tw, th
 terminal_size()
 
+
 def title():
   print(" * *"*int(width/4))
   print("~ THE MATH QUIZ ~".center(width))
   print(" * *"*int(width/4))
   print("\n")
 title()
+def clr():
+  os.system('clear')
+  title()
 print("What is your name?".center(width))
 inputname = True
 while inputname == True:
@@ -34,10 +38,42 @@ while inputname == True:
     print("Please enter a real name")
   elif name.isalpha():
     print("\n")
+    slep1()
     print(f"Welcome {name} to ~ THE MATH QUIZ ~".center(width))
     inputname = False
     slep2()
     clr()  
   else:
     print("Please enter a real name")
-    
+
+def choosediff():
+  print("Choose your difficulty".center(width))
+  print("1 = Easy       ".center(width))
+  print("2 = Normal     ".center(width))
+  print("3 = Hard       ".center(width))
+  print("4 = Extreme    ".center(width))
+  print("5 = IMPOSSIBLE ".center(width))
+choosediff()
+def chooseword():
+  global word
+  global levelchoosing
+  global lvlchoice
+  while levelchoosing == True:
+    lvlchoice = input()
+    if lvlchoice == "1":
+      word = random.choice(lvl1_words)
+      levelchoosing = False
+    elif lvlchoice == "2":
+      word = random.choice(lvl2_words)
+      levelchoosing = False
+    elif lvlchoice == "3":
+      word = random.choice(lvl3_words)
+      levelchoosing = False
+    elif lvlchoice == "4":
+      word = random.choice(lvl4_words)
+      levelchoosing = False
+    elif lvlchoice == "5":
+      word = random.choice(lvl5_words)
+      levelchoosing = False
+    else:
+      print("Please enter a number from 1 to 5")
