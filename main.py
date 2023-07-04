3b0e492f4a38e9050d49753c2b0287c451f7565f
TimePrecursor <131825199+TimePrecursor@users.noreply.github.com>
2023-07-04 01:21:39 +0000

    Added the answer_checker() function.
    Including some edits the formatting and question() function.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import fcntl, termios, struct
 import random
 import time
 import os
+lastscore = 0
+
 #from tabulate import tabulate
 def slep1():
   time.sleep(1)
@@ -12,19 +14,19 @@ def slep3():
 
 #Find terminal size
 def terminal_size():
-      th, tw, hp, wp = struct.unpack('HHHH',
-          fcntl.ioctl(0, termios.TIOCGWINSZ,
-          struct.pack('HHHH', 0, 0, 0, 0)))
-      global width
-      width = tw
-      return tw, th
+  th, tw, hp, wp = struct.unpack(
+    'HHHH', fcntl.ioctl(0, termios.TIOCGWINSZ, struct.pack('HHHH', 0, 0, 0,
+                                                           0)))
+  global width
+  width = tw
+  return tw, th
 terminal_size()
 
 #title layout
 def title():
-  print(" * *"*int(width/4))
+  print(" * *" * int(width / 4))
   print("~ THE MATH QUIZ ~".center(width))
-  print(" * *"*int(width/4))
+  print(" * *" * int(width / 4))
   print("\n")
 title()
 
@@ -40,21 +42,21 @@ def question():
   global pt2
   global pt3
   global answer_is
-  e = random.randint(1,3)
-  pt1 = random.randint(0,11)
-  pt2 = random.randint(0,11)
+  e = random.randint(1, 3)
+  pt1 = random.randint(0, 11)
+  pt2 = random.randint(0, 11)
   if e == 1:
     pt3 = "+"
-    answer_is = (pt1+pt2)
+    answer_is = (pt1 + pt2)
   elif e == 2:
     pt3 = "-"
-    answer_is = (pt1-pt2)
+    answer_is = (pt1 - pt2)
   elif e == 3:
     pt3 = "x"
-    answer_is = ((pt1)*(pt2))
+    pt1 = random.randint(0, 6)
+    pt2 = random.randint(0, 6)
+    answer_is = ((pt1) * (pt2))
   question1 = ((f"what is: {pt1} {pt3} {pt2}"))
-
-  
 
 #asks for name
 print("What is your name?".center(width))
@@ -70,7 +72,7 @@ while inputname == True:
     print(f"Welcome {name} to ~ THE MATH QUIZ ~".center(width))
     inputname = False
     slep2()
-    clr()  
+    clr()
   else:
     print("Please enter a real name")
 
@@ -78,12 +80,26 @@ print("\n")
 question()
 print(question1.center(width))
 checkanswer = True
-while checkanswer == True:
-  answer = str(input())
-  print(answer_is)  
-  if answer == str(answer_is):
-    print("correct")
-    checkanswer = False
-  else:
-    print("wrong")
-print("\nDONE")
+questiontries = 1
+
+
+def answer_checker():
+  global checkanswer
+  global questiontries
+  checkanswer = True
+  questiontries = 1
+  while checkanswer == True:
+    questiontries += 1
+    answer = str(input())
+    if answer == str(answer_is):
+      print("correct")
+      checkanswer = False
+    elif answer != str(answer_is) and questiontries < 3:
+      print("\n")
+      print("Wrong!  Last try!".center(width))
+    else:
+      print("\nWrong agian!  Next qeustion...".center(width))
+      checkanswer = False
+answer_checker()
+
+print("\n\nDONE")
